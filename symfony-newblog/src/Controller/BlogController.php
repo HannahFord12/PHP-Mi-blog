698d362e05f81661bb962e4f1a4d93adf7747476
HannahFord12 <isabelmanzana12@gmail.com>
2022-11-28 13:30:03 +0100

    Blog hasta antes de comentarios

---
+++ symfony-newblog/src/Controller/BlogController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+use Symfony\Component\String\Slugger\SluggerInterface;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
+use App\Entity\Post;
+use App\Form\PostFormType;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+
+class BlogController extends AbstractController
+{
+    #[Route('/blog/{page}', name: 'blog', requirements: ['page' => '\d+'])]
+    public function index(ManagerRegistry $doctrine, int $page = 1): Response
+    {
+        $repository = $doctrine->getRepository(Post::class);
+        $posts = $repository->findAll($page);
+    
+        return $this->render('blog/blog.html.twig', [
+            'posts' => $posts,
+        ]);
+    }
+    
+
+    #[Route('/single_post/{slug}', name: 'single_post')]
+    public function post(ManagerRegistry $doctrine, $slug): Response
+    {
+        $repository = $doctrine->getRepository(Post::class);
+        $post = $repository->findOneBy(["slug"=>$slug]);
+        $recents = $repository->findRecents();
+        return $this->render('blog/single_post.html.twig', [
+            'post' => $post,
+            'recents' => $recents
+        ]);
+    }
+
+
+    #[Route('/blog/new', name: 'new_post')]
+    public function newPost(ManagerRegistry $doctrine, Request $request, SluggerInterface $slugger): Response
+    {
+        $post = new Post();
+        $form = $this->createForm(PostFormType::class, $post);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $file = $form->get('image')->getData();
+            if ($file) {
+                $originalFilename = pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);
+                // this is needed to safely include the file name as part of the URL
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = $safeFilename.'-'.uniqid().'.'.$file->guessExtension();
+        
+                // Move the file to the directory where images are stored
+                try {
+        
+                    $file->move(
+                        $this->getParameter('images_directory'), $newFilename
+                    );
+        
+                } catch (FileException $e) {
+                    // ... handle exception if something happens during file upload
+                }
+        
+                // updates the 'file$filename' property to store the PDF file name
+                // instead of its contents
+                $post->setImage($newFilename);
+            }
+            $post = $form->getData();   
+            $post->setSlug($slugger->slug($post->getTitle()));
+            $post->setPostUser($this->getUser());
+            $post->setNumLikes(0);
+            $post->setNumComments(0);
+            $entityManager = $doctrine->getManager();    
+            $entityManager->persist($post);
+            $entityManager->flush();
+            return $this->redirectToRoute('single_post', ["slug" => $post->getSlug()]);
+        }
+        return $this->render('blog/new_post.html.twig', array(
+            'form' => $form->createView()    
+        ));
+    }
+
+    
+
+
+}
