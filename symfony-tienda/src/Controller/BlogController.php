ef1063ac94796a13ac21dfa93511275d015159a1
HannahFord12 <isabelmanzana12@gmail.com>
2022-12-07 10:18:44 +0100

    Teinda adelantada

--- symfony-tienda/src/Controller/BlogController.php
+++ symfony-tienda/src/Controller/BlogController.php
@@ -8,10 +8,10 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class BlogController extends AbstractController
 {
-    #[Route('/blog', name: 'app_blog')]
+    #[Route('/blog', name: 'blog')]
     public function index(): Response
     {
-        return $this->render('blog/index.html.twig', [
+        return $this->render('page/blog.html.twig', [
             'controller_name' => 'BlogController',
         ]);
     }
